7cfd95c92ce9c2dbe9a0ba286a1d27c7398b9761
xryuseix <ryusei143.shootingstar@gmail.com>
2019-12-22 20:31:51 +0900

    update at 2019-12-22 20:31:51.407998

--- storage/Header/xryuseix.h
+++ storage/Header/xryuseix.h
@@ -799,35 +799,23 @@ public:
 	// セグメント木を持つ配列
 	vector<int> dat;
 
-	SegmentTree(vi v) {
-		n = calc2(v.size());
-		vector<int> tmp(n*2 - 1);
-		dat = tmp;
-		for(int i = 0; i < dat.size(); i++) {
-			dat[i] = INF; // 最大値を求めるときはここを変える(1)
-		}
+	SegmentTree(vi &v) {
+		n = 1 << (int)ceil(log2(v.size()));
+		dat = vector<int>((n<<1) - 1);
 		for(int i = 0; i < v.size(); i++) {
 			update(i, v[i]);
 		}
-	}
-
-	// Nより大きい２のべき乗数nを計算
-	int calc2(int N){
-		int n = 1;
-		while(n < N) {
-			n *= 2;
+		for(int i = v.size(); i < n; i++) {
+			update(i, INF);
 		}
-		return n;
 	}
 
-	// k番目の値(0-indexed)をaに変更
-	void update(int k, int a) {
-		k += n - 1;
-		dat[k] = a;
-		//登りながら更新
-		while(k > 0) {
-			k = (k - 1)/2;
-			dat[k] = min(dat[k*2 + 1], dat[k*2 + 2]); // 最大値を求めるときはここを変える(2)
+	// k番目の値(0-indexed)をxに変更
+	void update(int k, int x) {
+		k += n;
+		dat[k - 1] = x;
+		for(k = k >> 1; k > 0; k >>= 1){
+			dat[k - 1] = min(dat[2*k - 1], dat[2*k]);
 		}
 	}
 
@@ -836,7 +824,7 @@ public:
 	int query(int a, int b, int k, int l, int r) {
 		// [a, b)と[l, r)が交差しなければINF
 		if(r <= a || b <= l) {
-			return INF; // 最大値を求めるときはここを変える(3)
+			return INF;
 		}
 		// [a, b)が[l, r)を完全に含んでいれば接点の値を返す
 		if(a <= l && r <= b) {
@@ -845,17 +833,20 @@ public:
 			// そうでなければ２つの子の最小値を再帰的に求める
 			int vl = query(a, b, k*2 + 1, l, (l + r)/2);
 			int vr = query(a, b, k*2 + 2, (l + r)/2, r);
-			return min(vl, vr); // 最大値を求めるときはここを変える(4)
+			return min(vl, vr);
 		}
 	}
+	
+}
 
 	void show() {
 		int ret = 2;
 		for(int i = 1; i <= 2*n - 1; i++) {
 			cout << dat[i - 1] << " ";
 			if(i == ret - 1) {
-				cout << "\n";
-				ret *= 2;
+				cout << "
+";
+				ret <<= 1;
 			}
 		}
 		cout << endl;
