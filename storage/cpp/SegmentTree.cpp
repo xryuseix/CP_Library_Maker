934a7584b571740c1fef9b7223e91fe1a954d9c3
xryuseix <ryusei143.shootingstar@gmail.com>
2020-01-18 15:16:15 +0900

    木ランダムアクセスができるようにした

--- storage/cpp/SegmentTree.cpp
+++ storage/cpp/SegmentTree.cpp
@@ -64,6 +64,13 @@ public:
 		}
 		return res;
 	}
+	int operator[](int k) {
+		// st[i]で添字iの要素の値を返す
+		if(k - n >= 0 || k < 0) {
+			return -INF;
+		}
+		return tree[tree.size()-n+k];
+	}
 
 	void show() {
 		int ret = 2;
