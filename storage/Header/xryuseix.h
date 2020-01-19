cdbacf5ac19ad66a02c9cf57d1150a7d1fdea696
xryuseix <ryusei143.shootingstar@gmail.com>
2020-01-20 02:52:41 +0900

    素因数分解なんかバグってた

--- storage/Header/xryuseix.h
+++ storage/Header/xryuseix.h
@@ -148,35 +148,17 @@ void cardano(double a, double b, double c, double d){
 		ans1 = x1; ans2 = x2; ans3 = x3;
 	}
 }
-map<int,int> prime;
-map<int,int>::iterator it;
-void factorize(int x){
-	int d, q;
-	bool ck = true;
-	while(x >= 4 && x%2 == 0){
-		x /= 2;
-		if(prime.find(2) == prime.end()) prime[2] = 1;
-		else prime[2]++;
+map<ll, ll> prime;
+void factorize(int n) {
+	for(int i = 2; i * i <= n; i++) {
+		while(n % i == 0) {
+			prime[i]++;
+			n /= i;
+		}
 	}
-	d = 3; q = x/d;
-	while(q >= d){
-		if(x%d == 0){
-			x = q;
-			if(prime.find(d) == prime.end()) prime[d] = 1;
-			else prime[d]++;
-		}
-		else if(ck){
-			d += 2;
-			ck = false;
-		}
-		else{
-			d += 4;
-			ck = true;
-		}
-		q = x/d;
+	if(n != 1) {
+		prime[n] = 1;
 	}
-	if(prime.find(x) == prime.end()) prime[x] = 1;
-	else prime[x]++;
 }
 bool isPrime(int x){
 	int i;
