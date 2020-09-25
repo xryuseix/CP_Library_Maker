2c9e6b1b882133c518e70e28b1a0a1b764969fa9
xryuseix <ryusei143.shootingstar@gmail.com>
2020-09-25 19:00:13 +0900

    update at 2020-09-25 19:00:12.690003

--- storage/cpp/SegmentTree.cpp
+++ storage/cpp/SegmentTree.cpp
@@ -5,16 +5,14 @@ class Sum {
 public:
 	// 単位元
 	T unit;
-	
+
 	Sum(void) {
 		// 単位元
 		unit = 0;
 	}
 
 	// 演算関数
-	T calc(T a, T b) {
-		return a + b; 
-	}
+	T calc(T a, T b) { return a + b; }
 };
 
 template <typename T>
@@ -22,16 +20,14 @@ struct Min {
 public:
 	// 単位元
 	T unit;
-	
+
 	Min(void) {
 		// 単位元
 		unit = INF;
 	}
 
 	// 演算関数
-	T calc(T a, T b) {
-		return min(a, b); 
-	}
+	T calc(T a, T b) { return min(a, b); }
 };
 
 template <typename T, class MONOID>
@@ -46,20 +42,34 @@ public:
 	// モノイド
 	MONOID mono;
 
-	SegmentTree(vector<T> &v) : n(1 << (int)ceil(log2(v.size()))), tree(vector<T>(n << 1)) {
-		for(int i = 0; i < v.size(); ++i) {
-			update(i, v[i]);
+	SegmentTree(vector<T>& v)
+		: n(1 << (int)ceil(log2(v.size()))),
+		tree(vector<T>(n << 1, mono.unit)) {
+		for (int i = 0; i < v.size(); ++i) {
+			tree[i + n] = v[i];
 		}
-		for(int i = v.size(); i < n; ++i) {
-			update(i, mono.unit);
+		for (int i = n - 1; i > 0; --i) {
+			tree[i] = mono.calc(tree[i << 1 | 0], tree[i << 1 | 1]);
 		}
 	}
+
+	SegmentTree(int _n)
+		: n(1 << (int)ceil(log2(_n))), tree(vector<T>(n << 1, mono.unit)) {}
 
 	// k番目の値(0-indexed)をxに変更
 	void update(int k, T x) {
 		k += n;
 		tree[k] = x;
-		for(k = k >> 1; k > 0; k >>= 1){
+		for (k = k >> 1; k > 0; k >>= 1) {
+			tree[k] = mono.calc(tree[k << 1 | 0], tree[k << 1 | 1]);
+		}
+	}
+
+	// k番目の値(0-indexed)をxを加算
+	void add(int k, T x) {
+		k += n;
+		tree[k] += x;
+		for (k = k >> 1; k > 0; k >>= 1) {
 			tree[k] = mono.calc(tree[k << 1 | 0], tree[k << 1 | 1]);
 		}
 	}
@@ -69,11 +79,11 @@ public:
 		T res = mono.unit;
 		l += n;
 		r += n;
-		while(l < r) {
-			if(l & 1) {
+		while (l < r) {
+			if (l & 1) {
 				res = mono.calc(res, tree[l++]);
 			}
-			if(r & 1) {
+			if (r & 1) {
 				res = mono.calc(res, tree[--r]);
 			}
 			l >>= 1;
@@ -84,18 +94,20 @@ public:
 
 	T operator[](int k) {
 		// st[i]で添字iの要素の値を返す
-		if(k - n >= 0 || k < 0) {
+		if (k - n >= 0 || k < 0) {
 			return -INF;
 		}
 		return tree[tree.size() - n + k];
 	}
 
-	void show(void) {
+	void show(int elements = 31) {
 		int ret = 2;
-		for(int i = 1; i < 2*n; ++i) {
-			if(tree[i] == mono.unit) cout << "UNIT ";
-			else cout << tree[i] << " ";
-			if(i == ret - 1) {
+		for (int i = 1; i < max(2 * n, elements); ++i) {
+			if (tree[i] == mono.unit)
+				cout << "UNIT ";
+			else
+				cout << tree[i] << " ";
+			if (i == ret - 1) {
 				cout << endl;
 				ret <<= 1;
 			}
