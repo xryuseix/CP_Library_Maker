63bbfde275a2ae0e0fb2cb3e26f166b8ad580286
xryuseix <ryusei143.shootingstar@gmail.com>
2020-12-20 16:59:58 +0900

    update bit

--- storage/Header/xryuseix.h
+++ storage/Header/xryuseix.h
@@ -1729,29 +1729,33 @@ template <typename T>
 class BIT {
 	int N;
 	vector<T> tree;
+
 public:
-	BIT(vector<T> &v) : N(v.size()), tree(vector<T>(v.size() + 1)) {
-		rep(i, v.size()) {
-			add(i, v[i]);
-		}
+	BIT(vector<T>& v) : N(v.size()), tree(vector<T>(v.size() + 1)) {
+		rep(i, v.size()) { add(i, v[i]); }
 	}
+	BIT(int n) : N(n), tree(vector<T>(n + 1)) {}
 
 	void add(int i, T x = 1) {
-		for (++i; i <= N; i += i&-i) {
+		for (++i; i <= N; i += i & -i) {
 			tree[i] += x;
 		}
 	}
 
-	T sum(int i) { // [0,i)の和
+	T sum(int l) {  // [0, l)の和
 		T x = 0;
-		for (; i; i -= i&-i) {
-			x += tree[i];
+		for (; l; l -= l & -l) {
+			x += tree[l];
 		}
 		return x;
 	}
 
-	T sum(int l, int r) { // [l,r)の和
+	T sum(int l, int r) {  // [l, r)の和
 		return sum(r) - sum(l);
+	}
+
+	T sum_back(int l) {  // [l, N)の和
+		return sum(N) - sum(l);
 	}
 };
 class LCA {
